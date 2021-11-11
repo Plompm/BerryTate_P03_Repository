5fd4c1e70f1ff8cf3230c0e47272066af40fa6e7
Plompm <taterileyberry@gmail.com>
2021-11-11 07:02:28 -0600

    Setting up strucutre an initial enemy

---
+++ BerryTate_P03/Assets/Scripts/EnemyManager_scr.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager_scr : MonoBehaviour
+{
+
+    enum _EState {Full, OneArm, OneLeg, OneArmOneLeg, NoLegs, NoArms, None};
+
+    _EState _EnemyS;
+
+    GameObject _LeftA;
+    GameObject _RightA;
+    GameObject _LeftL;
+    GameObject _RightL;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _EnemyS = _EState.Full;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
