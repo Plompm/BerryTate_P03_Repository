fdb3ce58288d0a84d04582322f089ba51e35beee
Plompm <taterileyberry@gmail.com>
2021-11-12 11:52:09 -0600

    re

--- BerryTate_P03/Assets/Scripts/EnemyManager_scr.cs
+++ BerryTate_P03/Assets/Scripts/EnemyManager_scr.cs
@@ -5,24 +5,54 @@ using UnityEngine;
 public class EnemyManager_scr : MonoBehaviour
 {
 
-    enum _EState {Full, OneArm, OneLeg, OneArmOneLeg, NoLegs, NoArms, None};
+    enum _EState { X, Y, Dash, Line, Dot};
 
     _EState _EnemyS;
 
-    GameObject _LeftA;
-    GameObject _RightA;
-    GameObject _LeftL;
-    GameObject _RightL;
+    [SerializeField] GameObject _LeftA;
+    [SerializeField] GameObject _RightA;
+    [SerializeField] GameObject _LeftL;
+    [SerializeField] GameObject _RightL;
+
+    int _EHealth;
+
+    float _speed;
 
     // Start is called before the first frame update
     void Start()
     {
-        _EnemyS = _EState.Full;
+        _EnemyS = _EState.X;
     }
 
     // Update is called once per frame
     void Update()
     {
+        _stateManger();
+        print(_EnemyS);
+    }
+
+    public void _EDamage(int _dmgAmount)
+    {
+        _EHealth -= _dmgAmount;
+    }
+
+    void _stateManger()
+    {
+        if (!_LeftA)
+        { 
         
+        }
+        if (!_RightA)
+        { 
+        
+        }
+        if (!_LeftL)
+        { 
+        
+        }
+        if (!_RightL)
+        { 
+            
+        }
     }
 }
---
+++ BerryTate_P03/Assets/Scripts/Limb_scr.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Limb_scr : MonoBehaviour
+{
+
+    int _LHealth = 100;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_LHealth <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void _LimbDamage(int _dmgAmount)
+    {
+        _LHealth -= _dmgAmount;
+    }
+}
